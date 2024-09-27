74a9430ccb48169b44bc24c0f38733cdd7cad3e4
Vinicius Rorato da Cruz <vinicius.cruz@novaagri.com.br>
2024-09-27 08:50:33 -0300

    adding functions of controller and users

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,7 +1,18 @@
+const users = require("../models/users")
+
 module.exports = {
     // POST /auth/register
     register: (req,res) => {
-        
+        const { name, email, password } = req.body
+        if(typeof name !=='string' || typeof email !=='string' || typeof password !=='string') {
+            return res.status(400).json({message:'Todos os campos são obrigatórios!'})
+        }
+
+        const existingUser = users.getUserByEmail(email)
+        if(existingUser) return res.status(400).json({message:'E-mail já cadastrado'})
+
+        const newUser = users.createUser(name, email, password)
+        res.status(201).json(newUser)
     }
     // POST /auth/login
 }
