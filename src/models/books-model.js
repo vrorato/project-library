67467fb293092caa9fef2a253580e440d1fc79ac
Vinicius Rorato da Cruz <vinicius.cruz@novaagri.com.br>
2024-09-27 14:39:27 -0300

    added loans controller and model, updated routes

--- src/models/books-model.js
+++ src/models/books-model.js
@@ -34,6 +34,16 @@ module.exports = {
         const deletedBook = books[bookIndex]
         books = books.filter(book => book.id !== id)
         return deletedBook
+    },
+    takeBook: (id) => {
+        const bookIndex = books.findIndex(book => book.id === id)
+        if(bookIndex === -1) throw new HttpError(404, 'Livro não encontrado!') 
+        books[bookIndex].quantityAvailable -= 1
+    },
+    returnBook: (id) => {
+        const bookIndex = books.findIndex(book => book.id === id)
+        if(bookIndex === -1) throw new HttpError(404, 'Livro não encontrado!') 
+        books[bookIndex].quantityAvailable += 1
     }
 
 
---
+++ src/models/loans-model.js
@@ -0,0 +1,52 @@
+const HttpError = require('../errors/HttpError')
+const booksModel = require('./books-model')
+
+const uuid = require('uuid').v4 //versao baseada em valores aleatorios
+const loans = [
+    {id:'1',userId:'1',bookId:'1',loanDate: new Date('2024-01-01'),returnDate: null,isReturned:false,isLate:true}
+]
+
+module.exports = {
+    getAllLoans: () => loans,
+    getLoanById: (id) => loans.find(loan => loan.id === id),
+    createLoan: (user, book) => {
+        if (book.quantityAvailable < 1) throw new HttpError(400, 'Não há exemplares disponíveis!')
+    
+        const today = new Date()
+        const returnDate = new Date()
+        returnDate.setDate(today.getDate() + 14)
+    
+        const newLoan = {
+          id: uuid(),
+          userId: user.id,
+          bookId: book.id,
+          loanDate: today,
+          returnDate: returnDate,
+          isReturned: false,
+          isLate: false
+        }
+    
+        loans.push(newLoan)
+        booksModel.takeBook(book.id)
+    
+        return newLoan
+      },
+    returnLoan: (id) => {
+        const loanIndex = loans.findIndex(loan => loan.id === id)
+        if(loanIndex === -1) throw new HttpError(404,'Empréstimo não encontrado!')
+
+        const loan = loans[loanIndex]
+        if(loan.isReturned) return null
+
+        loan.isReturned = true
+        const today = new Date()
+        loan.returnDate = today
+
+        const limitDate = new Date(loan.loanDate)
+        loan.isLate = today > limitDate
+        loan.returnDate = today
+
+        booksModel.returnBook(loan.bookId)
+        return loan
+    }
+}
